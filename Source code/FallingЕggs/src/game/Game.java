53d264f70785c368455d4101faf324d05137b9dd
geiff <geiff@mail.bg>
2016-04-17 13:30:28 +0300

    Add more entities

--- Source code/FallingЕggs/src/game/Game.java
+++ Source code/FallingЕggs/src/game/Game.java
@@ -6,18 +6,19 @@ import game.entities.Egg;
 import game.entities.Player;
 import game.entities.Stone;
 import gfx.Assets;
-import gfx.ImageLoader;
 import gfx.SpriteSheet;
 import display.Menu;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
-import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Game extends MouseInput implements Runnable {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final int MAX_X = 35;
     private String name;
     private int width, height;
 
@@ -25,22 +26,12 @@ public class Game extends MouseInput implements Runnable {
     private Thread thread; //нишка
     private boolean isRunning;
     public static boolean isEsc;
-    public static boolean isStop;
-    // public static boolean isPause;
-    // public static boolean isStart;
-
-
-    // private int l1 = -100;
-    // private int l2 = -300;
-    // private int l3 = -500;
-    //  private int l4 = -100;
-    private int col;
-    private Player rabbit;
+    protected Player rabbit;
     private ArrayList<Egg> eggs;
     private ArrayList<Ducky> duckies;
-    //  private Ducky ducky;
     private ArrayList<Stone> stones;
 
+    Random random = new Random();
     private InputHandler ih;
     private MouseInput mi;
 
@@ -51,13 +42,12 @@ public class Game extends MouseInput implements Runnable {
     private BufferedImage bckg; // game background
 
     private Menu menu;
+    private Stone stone;
 
-    public static enum STATE {
+    public enum STATE {
         MENU,
         GAME
     }
-
-    ;
 
     public static STATE State = STATE.MENU;
 
@@ -65,7 +55,6 @@ public class Game extends MouseInput implements Runnable {
         this.name = name;
         this.width = width;
         this.height = height;
-
     }
 
     public void init() {
@@ -73,111 +62,96 @@ public class Game extends MouseInput implements Runnable {
         this.display = new Display(this.name, this.width, this.height);
         this.ih = new InputHandler(this.display.getCanvas());
         this.rabbit = new Player(120, 450, 0);
-
         this.bckg = Assets.background; // initialize game background image
 
-        //  this.egg = new Egg(300, 450, 148, 125);
-        //
-        this.stones = new ArrayList<>();
-        stones.add(new Stone(250, 0, 27, 17, 5));
-        stones.add(new Stone(550, 0, 27, 17, 2));
-
         this.duckies = new ArrayList<>();
-        duckies.add(new Ducky(350, 0, 33, 36, 4));
-        duckies.add(new Ducky(600, 0, 33, 36, 6));
+        for (int i = 0; i < random.nextInt(20); i++) {
+            addDucky(new Ducky(random.nextInt(Game.WIDTH - Game.MAX_X), 0, 33, 36));
+        }
 
         this.eggs = new ArrayList<>();
-        eggs.add(new Egg(150, 0, 20, 25, 3));
-        eggs.add(new Egg(450, 0, 20, 25, 1));
+        for (int i = 0; i < random.nextInt(20); i++) {
+            addEgg(new Egg(random.nextInt(Game.WIDTH - Game.MAX_X), 0, 20, 25));
+        }
+
+        this.stones = new ArrayList<>();
+        for (int i = 0; i < random.nextInt(10); i++) {
+        addStone(new Stone(random.nextInt(Game.WIDTH - Game.MAX_X), 0, 27, 17));}
 
         menu = new Menu();
         this.mi = new MouseInput(this.display.getCanvas());
-        // this.addMouseListener(new MouseInput());
 
     }
 
     public void tick() { //визуализираме нещата
-        //  isPause = false;
-        //  isStart = false;
-        //  isEsc = false;
-
         if (State == STATE.GAME) {
             this.rabbit.tick();
+
             for (int i = 0; i < eggs.size(); i++) {
+                if (this.eggs.get(i).getY() >= (Game.HEIGHT)) {
+                    this.eggs.get(i).setY(0);
+                }
+
                 if (this.eggs.get(i) != null) {
                     this.eggs.get(i).tick();
                 }
             }
 
             for (int i = 0; i < duckies.size(); i++) {
+                if (this.duckies.get(i).getY() >= (Game.HEIGHT)) {
+                    this.duckies.get(i).setY(0);
+                }
+
                 if (this.duckies.get(i) != null) {
                     this.duckies.get(i).tick();
                 }
             }
 
             for (int i = 0; i < stones.size(); i++) {
-                if (this.stones.get(i) != null) {
-                    this.stones.get(i).tick();
+                if (stones.get(i).getY() >= (Game.HEIGHT)) {
+                    stones.get(i).setY(0);
+                }
+
+                if (stones.get(i) != null) {
+                    stones.get(i).tick();
                 }
             }
 
-            if (isEsc) {
-                System.exit(1);
-            }
-
-
-            //   if (isPause) {
-            //       this.stop();
-            //    //   isRunning = false;
-            //    //   isEsc = true;
-            //    //   isStart = false;
-
-
-            //       if (isStart) {
-            //           this.start();
-            //           //isRunning = true;
-            //          // isPause = false;
-            //         //  isEsc = true;
-            //       }
-            //   }
-
-
-            //   this.l1 += 5;
-            //  this.l2 += 7;
-            //  this.l3 += 10;
-            //  this.l4 = +20;
-
             for (int i = 0; i < eggs.size(); i++) {
                 if (this.eggs.get(i) != null && this.rabbit.boundingBox.intersects(this.eggs.get(i).boundingBox)) {
-                    this.rabbit.addDamega(5);
+                    this.rabbit.addDamega(1);
                     eggs.remove(this.eggs.get(i));
                 }
             }
 
+
             for (int i = 0; i < duckies.size(); i++) {
                 if (this.duckies.get(i) != null && this.rabbit.boundingBox.intersects(this.duckies.get(i).boundingBox)) {
-                    this.rabbit.addDamega(10);
+                    this.rabbit.addDamega(3);
                     duckies.remove(this.duckies.get(i));
                 }
             }
 
             for (int i = 0; i < stones.size(); i++) {
                 if (this.stones.get(i) != null && this.rabbit.boundingBox.intersects(this.stones.get(i).boundingBox)) {
-                    this.rabbit.takeDamega(20);
+                    this.rabbit.takeDamega(3);
                     stones.remove(this.stones.get(i));
                 }
             }
 
-            if (this.rabbit.hitPoints >= 30) {
+            if (this.rabbit.hitPoints >= 50 || (stones.size() + eggs.size() + duckies.size()) == 0) {
                 System.out.printf("\nCongratulation!\nYou can going home with %d eggs!", this.rabbit.hitPoints);
 
-               // isStop = true;
                 this.stop();
-            }else if (this.rabbit.hitPoints < 0){
+
+            } else if (this.rabbit.hitPoints < 0) {
                 System.out.printf("Sorry, all the eggs are broken!");
                 this.stop();
             }
 
+            if (isEsc) {
+                System.exit(1);
+            }
         }
     }
 
@@ -191,9 +165,6 @@ public class Game extends MouseInput implements Runnable {
         this.graphics = this.bufferStrategy.getDrawGraphics();
 
         this.graphics.clearRect(0, 0, this.width, this.height);
-
-        //Start drowing
-        //this.graphics.drawImage(ImageLoader.loadImage("/bng.jpg"), 0, 0, 800, 600, null);
 
         this.graphics.drawImage(this.bckg, 0, 0, 800, 600, null); // draw game background
 
@@ -209,10 +180,6 @@ public class Game extends MouseInput implements Runnable {
                 }
             }
 
-            //    if (this.egg != null) {
-            //       this.egg.render(this.graphics);
-            //   }
-
             for (int i = 0; i < duckies.size(); i++) {
                 if (this.duckies.get(i) != null) {
                     this.duckies.get(i).render(this.graphics);
@@ -221,33 +188,29 @@ public class Game extends MouseInput implements Runnable {
 
             for (int i = 0; i < stones.size(); i++) {
                 if (this.stones.get(i) != null) {
-                    this.stones.get(i).render(this.graphics);
+                    this.stones.get(i).render(graphics);
                 }
             }
-
-
         } else if (State == STATE.MENU) {
             menu.render(graphics);
         }
 
-      //  if (isEsc) {
-      //     System.exit(1);
-      // }
-        // this.graphics.setColor(Color.pink);
-        //  this.graphics.fillOval(100, this.l1, 20, 20);
-
-
-        // this.graphics.setColor(Color.red);
-        //  this.graphics.fillOval(200, this.l2, 20, 20);
-
-
-        // this.graphics.setColor(Color.green);
-        // this.graphics.fillOval(300, this.l3, 20, 20);
-        //End drawing
-
         this.graphics.dispose();
         this.bufferStrategy.show();
     }
+
+    public void addStone(Stone st) { //da se iznese w obshtiq klas
+        stones.add(st);
+    }
+
+    public void addEgg(Egg egg) { //da se iznese w obshtiq klas
+        eggs.add(egg);
+    }
+
+    public void addDucky(Ducky dc) { //da se iznese w obshtiq klas
+        duckies.add(dc);
+    }
+
 
     @Override
     public void run() {
@@ -261,11 +224,9 @@ public class Game extends MouseInput implements Runnable {
         long lastTimeTicked = System.nanoTime();
 
         while (isRunning) {
-            // for (int i = 0; i < 20; i++) {
             now = System.nanoTime();
             delta += (now - lastTimeTicked) / timePerFrame;
             lastTimeTicked = now;
-           // System.out.println(delta);
             System.out.printf("Your eggs are: %d\n", this.rabbit.hitPoints);
 
             if (delta >= 1) {
@@ -279,8 +240,6 @@ public class Game extends MouseInput implements Runnable {
                 render();
                 delta--;
             }
-            // }
-
         }
 
         this.stop(); //за да спрем нишката
--- Source code/FallingЕggs/src/game/entities/Egg.java
+++ Source code/FallingЕggs/src/game/entities/Egg.java
@@ -1,38 +1,52 @@
 package game.entities;
 
+import game.Game;
 import gfx.Assets;
 import gfx.ImageLoader;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Egg {
     private int x, y, width, height;
     public Rectangle boundingBox;
     private BufferedImage img;
-    private int z = y;
-    private int sleep;
+    Random random = new Random();
 
-    public Egg(int x, int y, int width, int height, int sleep) {
+
+    public Egg(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.boundingBox = new Rectangle(x, y, width, height);
         this.img = Assets.egg;
-        this.sleep = sleep;
+
     }
 
     public  void tick() {
 
-        //this.z += 5;
-        this.z += sleep;
+        this.y += random.nextInt(10); //mooving;
+        if(y >= (Game.HEIGHT)){
+            this.y = 0;
+            this.x = random.nextInt(Game.WIDTH - Game.MAX_X);
+        }
 
-        this.boundingBox.setBounds(this.x, this.z, this.width, this.height); //ъпдейдваме боиндинбокса
+
+        this.boundingBox.setBounds(this.x, this.y, this.width, this.height); //ъпдейдваме боиндинбокса
     }
 
 
     public void render(Graphics g){
-        g.drawImage(this.img, x, this.z, this.width, this.height, null);
+        g.drawImage(this.img, x, this.y, this.width, this.height, null);
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setY(int y){
+        this.y = y;
     }
 }
